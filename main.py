acbfc3acb0be4260a361b17accbe3a067023b374
Jeff-Gui <1964728070@qq.com>
2020-12-13 14:13:15 +0800

    I/O bug fix
    Also add command line visual aid

--- main.py
+++ main.py
@@ -17,22 +17,29 @@ import doTrack
 import summary
 
 def main(argv):
+    
+    # I/O variables: output & input directory; GFP/mCherry/DIC file path and mode
+    # The program supports two modes, 
+    #   1) Single input: user specify single GFP/mCherry/DIC image datasets.
+    #   2) Batch (directory input): user specify folder containing multiple datasets.
     out = ''
     ip = ''
     gfp_path = ''
     mCherry_path = ''
     dic_path = ''
     mode = ''
+    
+    # Internal variables: store I/O information, cnn model information, run switch.
     prefix = ''
     verbose = False
     gfp_list = []
     mcy_list = []
     dic_list = []
     cnn_path = basepath + '/data/model.h5'
+    
+    # Collect and resolve user command
     try:
-        opts, args = getopt.getopt(argv, "hbi:g:m:d:o:", ["indir=","GFP_image=","mCherry_image=","DIC_image=", "outdir="])
-        # h: switch-type parameter
-        # i: / o: parameter must with some values
+        opts, args = getopt.getopt(argv, "-h-i:-g:-m:-d:-o:-v", ["help","indir=","GFP_image=","mCherry_image=","DIC_image=", "outdir=", "verbose"])
     except getopt.GetoptError:
         print('fucci.py               -v <verbose> -h <help> \n ## Directory Mode ##  -i <input directory> -o <output directory> \n ##    File Mode   ##  -g <GFP image> -m <mCherry image> -d <DIC image> -o <output directory>')
         sys.exit()
@@ -49,12 +56,12 @@ def main(argv):
         elif opt in ("-o", "--outdir"):
            out = arg + "/"
         elif opt in ("-g", "--GFP_image"):
-           gfp_path = arg + "/"
+           gfp_path = arg
            mode = 'file'
         elif opt in ("-m", "--mCherry_image"):
-           mCherry_path = arg + "/"
+           mCherry_path = arg
         elif opt in ("-d", "--DIC_image"):
-           dic_path = arg + "/"
+           dic_path = arg
         elif opt in ("-v", "--verbose"):
             verbose = True
     if len(ip) * len(gfp_path) or len(ip) * len(mCherry_path) or len(ip) * len(dic_path) != 0:
@@ -63,6 +70,7 @@ def main(argv):
         sys.exit()
     
     if mode == 'dir':
+        # in batch / directory mode, resolve file names in the directory
         if verbose: print("Resolving input directory.")
         for filename in os.listdir(ip):
             if re.match('(.*GFP.*).tif', filename):
@@ -72,21 +80,27 @@ def main(argv):
             elif re.match('(.*DIC.*).tif', filename):
                 dic_list.append(filename)
     else:
+        # in single mode, extract file name from file path
         gfp_list.append(re.search('.*/(.*GFP.*)', gfp_path).group(1))
         mcy_list.append(re.search('.*/(.*mCherry.*)', mCherry_path).group(1))
         dic_list.append(re.search('.*/(.*DIC.*)', dic_path).group(1))
         
-    print("GFP images: " + str(gfp_list))
-    print("mCherry images" + str(mcy_list))
-    print("DIC images" + str(dic_list))
-    if len(set(len(gfp_list), len(mcy_list), len(dic_list))) != 1:
-        print("Error! Resolved datasets not of same length.")
+    # Wait for user to confirm file names.
+    print("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n")
+    print("GFP images:      " + str(gfp_list))
+    print("mCherry images:  " + str(mcy_list))
+    print("DIC images:      " + str(dic_list))
+    print("\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n")
+    if len(set([len(gfp_list), len(mcy_list), len(dic_list)])) != 1:
+        # image file should have one-to-one relationship
+        print("Error! Can not resolve datasets without same lengths.")
         sys.exit()
-    go = input("Image files in correct order? [y/n] ")
+    go = input("\nImage files in correct order? [y/n] ")
     if go!='y':
         sys.exit()
     
-    for i in len(gfp_list):
+    # Main functions
+    for i in range(len(gfp_list)):
         if mode == 'dir':
             gfp_path = ip + gfp_list[i]
             mCherry_path = ip + mcy_list[i]
@@ -114,6 +128,8 @@ def main(argv):
         # Step 5. Output result
         summary.plot_track(tracks, out, prefix)
         summary.save_track(tracks, out, prefix)
+    
+    return
     
 if __name__ == "__main__":
     main(sys.argv[1:])
